4bcd0b5dffe66ed9f3f1412f179eec0db06c341d
Arsenii Semenov <2014.semenow.senya@gmail.com>
2021-11-16 21:02:13 +0200

    Finish

--- Client/translator/src/Switcher.js
+++ Client/translator/src/Switcher.js
@@ -1,18 +1,10 @@
 import './Switcher.css';
 
 export default function Switcher(props) {
-
-    const changeDir = (e) => {
-        // props.setToRussian(true);
-        e.stopPropagation();
-        console.dir(e.target);
-        const switcher = document.getElementById('switcher');
-        console.log(switcher.style);
-        switcher.style = {transform: 'rotate(180deg)'};
-    }
+    const {rotated, setRotated} = props;
 
     return(
-        <div id="switcher" rotate='0' className="box" onClick={e => changeDir(e)}>
+        <div id="switcher" className={'box' + ((rotated) ? ' rotated' : '')} onClick={() => setRotated(!rotated)}>
             <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#145a8c" viewBox="0 0 16 16">
                 <path d="M0 14a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2v12zm4.5-6.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5a.5.5 0 0 1 0-1z"/>
             </svg>
--- Client/translator/src/TranslateBox.js
+++ Client/translator/src/TranslateBox.js
@@ -1,12 +1,46 @@
 import Switcher from './Switcher';
 import './TranslateBox.css';
+import {useEffect, useState} from 'react';
+import axios from 'axios';
+import { endpoint } from './config';
 
 export default function TranslateBox() {
+    const [rotated, setRotated] = useState(false);
+    const [inputText, setInputText] = useState('');
+    const [translatedText, setTranslatedText] = useState('');
+
+    useEffect(() => {
+        setInputText(translatedText);
+        setTranslatedText(inputText);
+    }, [rotated]);
+
+    useEffect(() => {
+        const delayDebounceFn = setTimeout(async () => {
+            await axios.post(endpoint + '/api/translator', {
+                from: (rotated) ? 'ru' : 'en',
+                to: (rotated) ? 'en' : 'ru',
+                text: inputText
+            }).then((res) => {
+                const {text} = res.data[0].translations[0];
+                setTranslatedText(text);
+            }).catch(() => {
+                setTranslatedText('Some error happened');
+            });
+
+
+            }, 300);
+            return () => clearTimeout(delayDebounceFn);
+    }, [inputText, rotated]);
+
+    const changeHandler = (e) => {
+        setInputText(e.target.value);
+    }
+
     return(
         <div className="container">
-            <label>English<input type="text"></input></label>
-            <Switcher></Switcher>
-            <label>Russian<input type="text"></input></label>
+            <label>English<textarea type="text" readOnly={rotated} onChange={(e) => changeHandler(e)} width='200px' height={100} value={(rotated) ? translatedText : inputText}></textarea></label>
+            <Switcher rotated={rotated} setRotated={setRotated}></Switcher>
+            <label>Russian<textarea type="text" readOnly={!rotated} onChange={(e) => changeHandler(e)} value={(rotated) ? inputText : translatedText}></textarea></label>
         </div>
     );
 }
